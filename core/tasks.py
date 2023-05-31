90c1d80ee99761610185e1ee7347bbec2eecf5c7
Connor Blackler <connor.blackler.ss@gmail.com>
2023-05-31 16:57:17 +0100

    [#1] {t:180} {personal_cv:main} updated website to use docker with github scraping, pull repo locally in a linux machine, clone repo and check for thumbnail file, updated this to custom 'scrape' command to populate models, front end loads models already

--- core/tasks.py
+++ core/tasks.py
@@ -6,71 +6,97 @@ import os
 from .models import GitRepo, Language
 
 
+import os
+import subprocess
+import requests
+
 github_username = 'Connor-Blackler'
 github_url = f'https://api.github.com/users/{github_username}/repos'
 file_path_in_repo = 'thumbnail.json'
 
 
-async def fetch_repo_detail(session, repo, headers):
-    file_url = f'https://api.github.com/repos/{github_username}/{repo}/contents/{file_path_in_repo}'
-    async with session.get(file_url, headers=headers) as file_response:
-        if file_response.status == 200:
-            file_data = await file_response.json()
-            file_content = base64.b64decode(
-                file_data['content']).decode('utf-8')
-            file_content = json.loads(file_content)
-            return {
+def _fetch_with_docker() -> list[dict]:
+    """
+    Testing power of docker on simple tasks like this
+    """
+    docker_username = os.environ.get('DOCKER_USERNAME')
+    docker_password = os.environ.get('DOCKER_PASSWORD')
+
+    # Log into Docker
+    subprocess.call(
+        ['docker', 'login', '-u', docker_username, '-p', docker_password])
+
+    # Get a list of all repositories for the given user
+    response = requests.get(github_url)
+    data = response.json()
+    repos = [repo['name'] for repo in data]
+
+    # Build Docker image, ensure no-cache in case of docker build caching
+    dockerfile_path = os.path.abspath(os.path.dirname(__file__))
+    subprocess.call(['docker', 'build', '-t',
+                    'my_docker_image', dockerfile_path])
+
+    ret = []
+    for repo in repos:
+        # Clone the repository
+        clone_command = ['docker', 'run', "--net=host", '-dit',
+                         '--name', f'docker_container_{repo}', 'my_docker_image', "/bin/sh"]
+
+        subprocess.check_output(
+            clone_command, stderr=subprocess.STDOUT, universal_newlines=True)
+
+        git_clone_command = [
+            'docker', 'exec', "-it", f'docker_container_{repo}', 'git', 'clone', f'https://github.com/{github_username}/{repo}.git']
+        git_clone_command_result = subprocess.run(
+            git_clone_command, capture_output=True)
+        print("Git clone:")
+        print(git_clone_command_result.stdout.decode('utf-8'))
+
+        check_file_command = [
+            'docker', 'exec', "-it", f'docker_container_{repo}', 'test', '-f', f"{repo}/{file_path_in_repo}"]
+        file_exists = subprocess.call(check_file_command) == 0
+
+        if file_exists:
+            cat_command = [
+                'docker', 'exec', "-it", f'docker_container_{repo}', 'cat', f"{repo}/{file_path_in_repo}"]
+            file_content = subprocess.check_output(cat_command).decode('utf-8')
+
+            print(f'Content of {file_path_in_repo} in {repo}:')
+            print(file_content)
+            ret.append({
                 'repo': repo,
                 'file_content': file_content,
-                'url': f"https://api.github.com/repos/{github_username}/{repo}"
-            }
+                'url': f"https://github.com/{github_username}/{repo}"
+            })
+        else:
+            print(f'The file {file_path_in_repo} does not exist in {repo}.')
+        subprocess.call(['docker', 'rm', '-f', f'docker_container_{repo}'])
 
-
-async def get_repo_details():
-    headers = {
-        'Authorization': os.environ.get('GITHUB_ACCESS_TOKEN'),
-    }
-
-    print("about to start")
-    print(headers)
-    async with aiohttp.ClientSession() as session:
-        response = await session.get(github_url, headers=headers)
-        data = await response.json()
-        repos = [repo['name'] for repo in data]
-
-        tasks = [fetch_repo_detail(session, repo, headers) for repo in repos]
-        repo_data = await asyncio.gather(*tasks)
-
-        # Remove None values in case of failed requests
-        repo_data = [data for data in repo_data if data is not None]
-        print(repo_data)
-        return repo_data
-
-
-def get_repo_thumnails() -> dict:
-    loop = asyncio.get_event_loop()
-    repo_data = loop.run_until_complete(get_repo_details())
-
-    return repo_data
+    return ret
 
 
 def scrape_and_save():
     print("lets scrape")
-    scraped_data = get_repo_thumnails()
+    scraped_data = _fetch_with_docker()
 
     for item in scraped_data:
         print(item)
+        file_content = json.loads(item["file_content"])
+
         this_repo, created = GitRepo.objects.get_or_create(
             url=item["url"]
         )
 
-        for this_lang in item["file_content"]["language"]:
+        for index, this_lang in enumerate(file_content["language"]):
             lang, created = Language.objects.get_or_create(
-                title=this_lang["title"]
+                title=this_lang
             )
-            lang.color = this_lang["language_bg"]
+            lang.color = file_content["language_bg"][index]
+            lang.save()
+
             this_repo.languages.add(lang)
 
-        this_repo.image = item["file_content"]["image"]
-        this_repo.description = item["file_content"]["description"]
+        this_repo.title = file_content["title"]
+        this_repo.image = file_content["image"]
+        this_repo.description = file_content["description"]
         this_repo.save()
--- generator/generator.py
+++ generator/generator.py
@@ -1,16 +1,13 @@
 import os
-import aiohttp
-import asyncio
 import subprocess
-import base64
-import json
+import requests
 
 github_username = 'Connor-Blackler'
 github_url = f'https://api.github.com/users/{github_username}/repos'
 file_path_in_repo = 'thumbnail.json'
 
 
-def _fetch_with_docker() -> dict:
+def _fetch_with_docker() -> list[dict]:
     """
     Testing power of docker on simple tasks like this
     """
@@ -26,71 +23,49 @@ def _fetch_with_docker() -> dict:
     data = response.json()
     repos = [repo['name'] for repo in data]
 
-    # Build Docker image, ensure no-cache incase of docker build caching
+    # Build Docker image, ensure no-cache in case of docker build caching
     dockerfile_path = os.path.abspath(os.path.dirname(__file__))
-    subprocess.call(['docker', 'build', '--no-cache', '-t',
+    subprocess.call(['docker', 'build', '-t',
                     'my_docker_image', dockerfile_path])
 
+    ret = []
     for repo in repos:
-        subprocess.call(['docker', 'run', '-d', '--name', f'docker_container_{repo}', 'my_docker_image',
-                         '/bin/sh', '-c', f'git clone https://github.com/{github_username}/{repo}.git'])
+        # Clone the repository
+        clone_command = ['docker', 'run', "--net=host", '-dit',
+                         '--name', f'docker_container_{repo}', 'my_docker_image', "/bin/sh"]
+
+        subprocess.check_output(
+            clone_command, stderr=subprocess.STDOUT, universal_newlines=True)
+
+        git_clone_command = [
+            'docker', 'exec', "-it", f'docker_container_{repo}', 'git', 'clone', f'https://github.com/{github_username}/{repo}.git']
+        git_clone_command_result = subprocess.run(
+            git_clone_command, capture_output=True)
+        print("Git clone:")
+        print(git_clone_command_result.stdout.decode('utf-8'))
 
         check_file_command = [
-            'docker', 'exec', f'docker_container_{repo}', 'test', '-f', f'{repo}/{file_path_in_repo}']
+            'docker', 'exec', "-it", f'docker_container_{repo}', 'test', '-f', f"{repo}/{file_path_in_repo}"]
         file_exists = subprocess.call(check_file_command) == 0
 
         if file_exists:
             cat_command = [
-                'docker', 'exec', f'docker_container_{repo}', 'cat', f'{repo}/{file_path_in_repo}']
+                'docker', 'exec', "-it", f'docker_container_{repo}', 'cat', f"{repo}/{file_path_in_repo}"]
             file_content = subprocess.check_output(cat_command).decode('utf-8')
 
             print(f'Content of {file_path_in_repo} in {repo}:')
             print(file_content)
+            ret.append({
+                'repo': repo,
+                'file_content': file_content,
+                'url': f"https://api.github.com/repos/{github_username}/{repo}"
+            })
         else:
             print(f'The file {file_path_in_repo} does not exist in {repo}.')
         subprocess.call(['docker', 'rm', '-f', f'docker_container_{repo}'])
 
-
-async def fetch_repo_detail(session, repo, headers):
-    file_url = f'https://api.github.com/repos/{github_username}/{repo}/contents/{file_path_in_repo}'
-    async with session.get(file_url, headers=headers) as file_response:
-        if file_response.status == 200:
-            file_data = await file_response.json()
-            file_content = base64.b64decode(
-                file_data['content']).decode('utf-8')
-            file_content = json.loads(file_content)
-            return {
-                'repo': repo,
-                'file_content': file_content,
-                'url': f"https://api.github.com/repos/{github_username}/{repo}"
-            }
-
-
-async def get_repo_details():
-    headers = {
-        'Authorization': os.environ.get('GITHUB_ACCESS_TOKEN'),
-    }
-
-    async with aiohttp.ClientSession() as session:
-        response = await session.get(github_url, headers=headers)
-        data = await response.json()
-        repos = [repo['name'] for repo in data]
-
-        tasks = [fetch_repo_detail(session, repo, headers) for repo in repos]
-        repo_data = await asyncio.gather(*tasks)
-
-        # Remove None values in case of failed requests
-        repo_data = [data for data in repo_data if data is not None]
-
-        return repo_data
-
-
-def get_repo_thumnails() -> dict:
-    loop = asyncio.get_event_loop()
-    repo_data = loop.run_until_complete(get_repo_details())
-
-    return repo_data
+    return ret
 
 
 if __name__ == "__main__":
-    print(get_repo_thumnails())
+    print(_fetch_with_docker())
