619e4574e13b8b5527465c07ca3222f6337d84d9
Connor Blackler <connor.blackler.ss@gmail.com>
2023-06-07 23:41:56 +0100

    [#1] {t:240} {personal_cv:main} converted to public postgres database

--- core/models.py
+++ core/models.py
@@ -13,10 +13,10 @@ class Language(models.Model):
 
 class GitRepo(models.Model):
     """Emulates a Git repository"""
-    url = models.URLField(blank=False, primary_key=True)
-    title = models.CharField(max_length=100, blank=False, default='')
+    url = models.URLField(max_length=500, blank=False, primary_key=True)
+    title = models.CharField(max_length=200, blank=False, default='')
     description = models.CharField(max_length=500, blank=False, default='')
-    image = models.URLField(blank=False, default='')
+    image = models.URLField(max_length=500, blank=False, default='')
     languages = models.ManyToManyField(
         Language, blank=True, related_name='repos')
 
@@ -33,12 +33,12 @@ EDUCATION_STATE = (
 
 class Education(models.Model):
     """A model for an education"""
-    title = models.CharField(max_length=100, blank=False, default='')
+    title = models.CharField(max_length=200, blank=False, default='')
     description = models.CharField(max_length=500, blank=False, default='')
-    url = models.URLField(blank=True, default='')
-    image = models.URLField(blank=False, default='')
+    url = models.URLField(max_length=500, blank=True, default='')
+    image = models.URLField(max_length=500, blank=False, default='')
     state = models.CharField(choices=EDUCATION_STATE, max_length=100)
-    certificate = models.URLField(blank=True, default='')
+    certificate = models.URLField(max_length=500, blank=True, default='')
     id = models.UUIDField(default=uuid.uuid4, unique=True,
                           primary_key=True, editable=False)
 
